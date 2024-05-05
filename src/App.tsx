7108280b573b8dee4055aae047c29e86d50aad95
조민기 <58856846+Nail0606@users.noreply.github.com>
2024-05-05 19:26:48 +0900

    Feat : 로그인, 회원가입 페이지 연결

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
-import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import LoginPage from "./Components/Login/LoginPage";
+import { useNavigate } from "react-router-dom";
+import RegisterPage from "./Components/Register/RegisterPage";
 
 function App() {
-  return <div className="App"></div>;
+  const navi = useNavigate();
+
+  return (
+    <div>
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
