5c2744bfe060cab5dd8e992f7495e8404ffb8495
조민기 <58856846+Nail0606@users.noreply.github.com>
2024-05-08 00:18:00 +0900

    Feat : 로그인 POST 기능 추가(토큰 or 세션은 미정)

--- src/Components/Login/LoginInput.tsx
+++ src/Components/Login/LoginInput.tsx
@@ -2,14 +2,26 @@ interface Props {
   label: string;
   placeholder: string;
   value: string;
+  setValue: (value: string) => void;
   type: "text" | "password";
 }
 
-export default function LoginInput({ label, placeholder, type, value }: Props) {
+export default function LoginInput({
+  label,
+  placeholder,
+  type,
+  value,
+  setValue,
+}: Props) {
   return (
     <div className="flex flex-col">
       <label htmlFor={label}>{label} </label>
-      <input placeholder={placeholder} type={type} value={value} />
+      <input
+        placeholder={placeholder}
+        type={type}
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+      />
     </div>
   );
 }
