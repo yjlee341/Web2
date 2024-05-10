c06ac5e5d7f1694889e3898b97e56a22822c776f
조민기 <58856846+Nail0606@users.noreply.github.com>
2024-05-11 03:50:11 +0900

    Feat : 로그인 에러 핸들링 추가

--- src/Hooks/useLoginUser.ts
+++ src/Hooks/useLoginUser.ts
@@ -1,12 +1,13 @@
 import { useMutation } from "@tanstack/react-query";
+import { useState } from "react";
 
 interface loginData {
   email: string;
   password: string;
 }
 
-const loginUser = async (loginData: loginData): Promise<void> => {
-  const response = await fetch(
+const fetchLogin = (loginData: loginData) => {
+  const response = fetch(
     "https://dbe5fa4c-8754-4548-a440-4d22b98d7740.mock.pstmn.io",
     {
       method: "POST",
@@ -15,13 +16,21 @@ const loginUser = async (loginData: loginData): Promise<void> => {
       },
       body: JSON.stringify(loginData),
     }
-  );
-
-  if (!response.ok) {
-    throw new Error("Network response was not ok");
-  }
+  ).then((response) => {
+    if (!response.ok) throw new Error("err");
+  });
+  return response;
 };
 
-export function useLoginUser() {
-  return useMutation({ mutationFn: loginUser });
-}
+export const useLoginUser = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const { mutate } = useMutation({
+    mutationFn: () => fetchLogin({ email, password }),
+    onError() {
+      alert("아이디나 비밀번호를 잘못 입력하였습니다.");
+    },
+  });
+
+  return { mutate, email, setEmail, password, setPassword };
+};
